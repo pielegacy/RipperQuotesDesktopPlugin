6d6cbb88ea48cb067eddbe5350bce60bf2ee7600
pielegacy <alex_billson@outlook.com>
2016-03-08 18:42:39 +1100

    It actually works

--- RipperQuotesDesktopPlugin/MainWindow.xaml.cs
+++ RipperQuotesDesktopPlugin/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,9 +23,13 @@ namespace RipperQuotesDesktopPlugin
     /// </summary>
     public partial class MainWindow : Window
     {
+        QuoteReader qr = new QuoteReader();
+        List<Quotes> quote_list = new List<Quotes>();
+        List<Topic> topic_list = new List<Topic>();
         public MainWindow()
         {
             InitializeComponent();
+            quote_topic.Visibility = Visibility.Collapsed;
         }
         private void ClearQuote(object sender, RoutedEventArgs e)
         {
@@ -30,6 +37,7 @@ namespace RipperQuotesDesktopPlugin
             {
                 quote_content.Text = "";
             }
+            FillTopics();
         }
 
         private void ClearAuthor(object sender, RoutedEventArgs e)
@@ -37,6 +45,54 @@ namespace RipperQuotesDesktopPlugin
             if (quote_author.Text == "The author...")
             {
                 quote_author.Text = "";
+            }
+            FillTopics();
+        }
+        public void FillTopics()
+        {
+            if (quote_topic.Items.Count == 0)
+            {
+                qr.DownloadQuotes();
+                foreach (var t in qr.topics())
+                {
+                    quote_topic.Items.Add(t.TopicName);
+                }
+            }
+            if (quote_topic.Items.Count > 0)
+            {
+                quote_topic.Visibility = Visibility.Visible;
+                quote_list = qr.quotes();
+                topic_list = qr.topics();
+            }
+        }
+        private void ShowSubmit(object sender, SelectionChangedEventArgs e)
+        {
+            quote_submit.Visibility = Visibility.Visible;
+        }
+
+        private void LoadShit(object sender, EventArgs e)
+        {
+            FillTopics();
+        }
+        private void SubmitQuote(object sender, RoutedEventArgs e)
+        {
+            Quotes quote = new Quotes();
+            quote = new Quotes()
+            {
+                QuoteId = quote_list[0].QuoteId + 1,
+                TopicId = topic_list.Find(t => t.TopicName == quote_topic.SelectedValue.ToString()).TopicId,
+                Topic = topic_list.Find(t => t.TopicName == quote_topic.SelectedValue.ToString()),
+                QuoteText = quote_content.Text,
+                QuoteAuthor = quote_author.Text
+
+            };
+            string quote_string = JsonConvert.SerializeObject(quote);
+            using (var client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                client.UploadString("http://ripperquotes.azurewebsites.net/api/QuotesApi", "POST", quote_string);
+                MessageBoxResult success = MessageBox.Show("Quote Posted");
+                this.Close();
             }
         }
     }
---
+++ RipperQuotesDesktopPlugin/QuoteBase.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace RipperQuotesDesktopPlugin
+{
+    public class QuoteBase
+    {
+
+    }
+    public class QuoteReader
+    {
+        private static Uri q_url = new Uri("http://ripperquotes.azurewebsites.net/api/QuotesApi");
+        private static Uri t_url = new Uri("http://ripperquotes.azurewebsites.net/api/TopicsApi");
+        public static List<Quotes> q_list = new List<Quotes>();
+        public static List<Topic> t_list = new List<Topic>();
+        public List<Quotes> pulled_quote = new List<Quotes>();
+
+        public async void DownloadQuotes()
+        {
+            using (HttpClient http = new HttpClient())
+            {
+                HttpResponseMessage q_response = await http.GetAsync(q_url);
+                var q_json = await q_response.Content.ReadAsStringAsync();
+                q_list = JsonConvert.DeserializeObject<List<Quotes>>(q_json);
+                HttpResponseMessage t_response = await http.GetAsync(t_url);
+                var t_json = await t_response.Content.ReadAsStringAsync();
+                t_list = JsonConvert.DeserializeObject<List<Topic>>(t_json);
+
+            }
+        }
+        public List<Quotes> quotes()
+        {
+            return q_list;
+        }
+        public List<Topic> topics()
+        {
+            return t_list;
+        }
+        public Dictionary<int, string> quotes_dictionary()
+        {
+            Dictionary<int, string> qd = new Dictionary<int, string>();
+            foreach (var q in q_list)
+            {
+                qd.Add(q.QuoteId, q.QuoteText);
+            }
+            qd.OrderByDescending(q => q.Key);
+            return qd;
+        }
+        // Pull //
+        public async void PullQuote(int id)
+        {
+            using (HttpClient http = new HttpClient())
+            {
+                HttpResponseMessage response = await http.GetAsync(new Uri("http://ripperquotes.azurewebsites.net/api/QuotesApi/" + id));
+                var json = await response.Content.ReadAsStringAsync();
+                pulled_quote = JsonConvert.DeserializeObject<List<Quotes>>(json);
+            }
+        }
+
+
+    }
+    public class Quotes
+    {
+        public int QuoteId { get; set; }
+        public int TopicId { get; set; }
+        public string QuoteText { get; set; }
+        public string QuoteAuthor { get; set; }
+        public Topic Topic { get; set; }
+        public string QuoteEffect { get; set; }
+    }
+    //Topic Model//
+    public class Topic
+    {
+        public int TopicId { get; set; }
+        public string TopicName { get; set; }
+        public string TopicDescription { get; set; }
+        public int TopicAmount { get; set; }
+    }
+}
